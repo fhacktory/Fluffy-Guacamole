09b296bbcd90226cd808524ed3817bb644c65491
MarionCrp <freja_sennyo@hotmail.com>
2016-03-12 16:37:53 +0100

    toto

--- web/db/migrate/20160312151628_create_benders.rb
+++ web/db/migrate/20160312151628_create_benders.rb
@@ -1,8 +1,8 @@
 class CreateBenders < ActiveRecord::Migration[5.0]
   def change
     create_table :benders do |t|
-      t.started_at
-      t.ended_at
+      t.datetime :started_at
+      t.datetime :ended_at
       t.timestamps
     end
   end
